2ec9c07362cb92af0b2c69bb43530bc9a0e6a0b0
wjk <977865769@qq.com>
2017-07-09 00:56:51 +0800

    update

--- server/routers/api.js
+++ server/routers/api.js
@@ -1,10 +1,11 @@
 /**
  * Created by Administrator on 2017/6/29 0029.
  */
-const router = require('koa-router')()
-const searchController = require('./../controllers/search')
-
+const router = require('koa-router')();
+const searchController = require('./../controllers/search');
+const aiqiyiController=require('./../controllers/aiqiyi');
 const routers = router
     .get('/search/:keyword', searchController.search)
+    .get('/search/1/:keyword',aiqiyiController.search)
 
-module.exports = routers
+module.exports = routers;
